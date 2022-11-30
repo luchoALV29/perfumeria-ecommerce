d3303b73b07ec1ee3331c4200dff6f0020aef690
Luciano <lucianoalvarez852@gmail.com>
2022-11-30 07:47:52 -0300

    Added list component

---
+++ src/components/List/List.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const List = ({categorias}) => {
+    return (
+        <div class="w-25 p-3">
+            <h5>Categorías Perfumes</h5>
+            <ul class="list-group">
+                {
+                    categorias.map((categoria) => {
+                        return <button type="button" class="list-group-item list-group-item-action">{categoria}</button>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+List.proptype = {
+    categorias: PropTypes.array.isRequired
+}
+
+export default List
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,24 +9,25 @@ const Navbar = () => {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">PERFUMES <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">MAKE UP</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
+                            SKIN CARE
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">FACIAL</a>
+                            <a class="dropdown-item" href="#">CORPORAL</a>
+                            <a class="dropdown-item" href="#">SOLARES</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">Ver más</a>
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">BENEFICIOS</a>
                     </li>
                 </ul>
             </div>
